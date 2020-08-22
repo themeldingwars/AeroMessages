f4247b2e2e2da7e636dfc9feab151cd39f4d72d1
Xsear <Xsear@users.noreply.github.com>
2020-08-22 21:21:11 +0200

    Vehicle abilities are on the VCC, not VBC, obviously. Add respective responses.

--- Scripts/GSS/GSS_Vehicle_CombatController_DeactivateAbility.cs
+++ Scripts/GSS/GSS_Vehicle_CombatController_DeactivateAbility.cs
@@ -1,19 +1,17 @@
 using Bitter;
 namespace PacketPeepScript
 {
-    [Script(MessageType.GSS, 27, 87, false)]
-    public class VehicleBaseControllerActivateAbility : BaseScript
+    [Script(MessageType.GSS, 28, 88, false)]
+    public class VehicleCombatControllerDectivateAbility : BaseScript
     {
         public uint Time;
-        public byte Unk_AbilityIdx; // 0x05 == Honk, 0x08 == SIN
-        public byte[] Unk3; // 0x00, 0x00 for Honk
+        public byte Unk_AbilityIdx; // 0x05 == Honk
 
         public override void Read(Bitter.BinaryStream Stream)
         {
             Stream.ByteOrder = BinaryStream.Endianness.LittleEndian;
             Time = Stream.Read.UInt();
             Unk_AbilityIdx = Stream.Read.Byte();
-            Unk3 = Stream.Read.ByteArray(2);
         }
     }
 }
