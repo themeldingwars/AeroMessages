c86ed17a4efca44e7b45018e27f9a8d0ccf5201a
Xsear <Xsear@users.noreply.github.com>
2020-08-26 21:26:28 +0200

    CBC UpdateChatPartyMembers and CBC SalvageRequest

---
+++ Scripts/GSS/GSS_Character_BaseController_UpdateChatPartyMembers.cs
@@ -0,0 +1,46 @@
+using Bitter;
+using System;
+namespace PacketPeepScript
+{
+    [Script(MessageType.GSS, 2, 185, false)]
+    public class CharacterBaseControllerUpdateChatPartyMembers : BaseScript
+    {
+        public byte[] Unk1;
+        public string InvitedCharacterName;
+        public byte Unk2; // I'm seeing 0b, it could be chat channel for squad
+        public byte[] InvitingEntity;
+        
+
+        public override void Read(Bitter.BinaryStream Stream)
+        {
+            Stream.ByteOrder = BinaryStream.Endianness.LittleEndian;
+
+            Unk1 = Stream.Read.ByteArray(9);
+
+            InvitedCharacterName = Stream.Read.String(GetNullTerminatedStrSize(Stream));
+
+            Unk2 = Stream.Read.Byte();
+
+            InvitingEntity = Stream.Read.ByteArray(8);
+
+        }
+
+        // Reads until we find 0x00, then resets the head and returns the number of bytes read.
+        private int GetNullTerminatedStrSize(Bitter.BinaryStream Stream)
+        {
+            long StartOffset = Stream.baseStream.ByteOffset;
+            do
+            {
+                byte b = Stream.Read.Byte();
+                if (b == 0x00)
+                {
+                    break;
+                }
+            }
+            while (Stream.baseStream.ByteOffset < Stream.baseStream.Length);
+            long EndOffset = Stream.baseStream.ByteOffset;
+            Stream.baseStream.ByteOffset = StartOffset;
+            return (int)(EndOffset - StartOffset);
+        }
+    }
+}
