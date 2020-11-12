b997ab1e02db250b7141b07090939bd4afbb2d8b
Xsear <Xsear@users.noreply.github.com>
2020-11-13 00:09:15 +0100

    Further Matrix_EnterZone

--- Scripts/Matrix/Matrix_EnterZone.cs
+++ Scripts/Matrix/Matrix_EnterZone.cs
@@ -1,4 +1,5 @@
 using Bitter;
+using System;
 namespace PacketPeepScript
 {
     [Script(MessageType.Matrix, 37)]
@@ -13,30 +14,41 @@ namespace PacketPeepScript
         public byte HotfixLevel;
         public ulong MatchId;
         public byte[] Unk2;
+        public uint Unk3;
         public string ZoneName;
-        public byte Unk3;
+        public byte Unk4;
         public byte[] Unk_ZoneTime;
-        public byte[] Unk4;
+        public DateTimeOffset Unk5_MicroUnix;
+        public DateTimeOffset Unk6_MicroUnix;
+        public byte[] Unk7;
+        public float Timescale;
+        public byte[] Unk8;
         public byte SpectatorModeFlag;
 
         public override void Read(Bitter.BinaryStream Stream)
         {
             Stream.ByteOrder = BinaryStream.Endianness.LittleEndian;
-
-            InstanceId = Stream.Read.ULong();
-            ZoneId = Stream.Read.UInt();
-            ZoneTimestamp = Stream.Read.ULong();
-            PreviewModeFlag = Stream.Read.Byte();
-            ZoneOwner = Stream.Read.String(GetNullTerminatedStrSize(Stream));
-            Unk1 = Stream.Read.ByteArray(6);
-            HotfixLevel = Stream.Read.Byte();
-            MatchId = Stream.Read.ULong();
-            Unk2 = Stream.Read.ByteArray(5);
-            ZoneName = Stream.Read.String(GetNullTerminatedStrSize(Stream));
-            Unk3 = Stream.Read.Byte();
-            Unk_ZoneTime = Stream.Read.ByteArray(16);
-            Unk4 = Stream.Read.ByteArray(41);
-            SpectatorModeFlag = Stream.Read.Byte();
+            if (true) {
+                InstanceId = Stream.Read.ULong();
+                ZoneId = Stream.Read.UInt();
+                ZoneTimestamp = Stream.Read.ULong();
+                PreviewModeFlag = Stream.Read.Byte();
+                ZoneOwner = Stream.Read.String(GetNullTerminatedStrSize(Stream));
+                Unk1 = Stream.Read.ByteArray(6);
+                HotfixLevel = Stream.Read.Byte();
+                MatchId = Stream.Read.ULong();
+                Unk2 = Stream.Read.ByteArray(1);
+                Unk3 = Stream.Read.UInt();
+                ZoneName = Stream.Read.String(GetNullTerminatedStrSize(Stream));
+                Unk4 = Stream.Read.Byte();
+                Unk_ZoneTime = Stream.Read.ByteArray(16);
+                Unk5_MicroUnix = Stream.Read.MicroUnixToMillis();
+                Unk6_MicroUnix = Stream.Read.MicroUnixToMillis();
+                Unk7 = Stream.Read.ByteArray(4);
+                Timescale = Stream.Read.Float();
+                Unk8 = Stream.Read.ByteArray(17);
+                SpectatorModeFlag = Stream.Read.Byte();
+            }
         }
 
         // Reads until we find 0x00, then resets the head and returns the number of bytes read.
@@ -55,6 +67,16 @@ namespace PacketPeepScript
             long EndOffset = Stream.baseStream.ByteOffset;
             Stream.baseStream.ByteOffset = StartOffset;
             return (int)(EndOffset - StartOffset);
+        } 
+    }
+
+    public static class MyExtensions
+    {
+        public static DateTimeOffset MicroUnixToMillis(this Bitter.BinaryReader R)
+        {
+            ulong Micros = R.ULong();
+            ulong Millis = Micros / 1000;
+            return DateTimeOffset.FromUnixTimeMilliseconds((long) Millis);
         }
     }
 }
