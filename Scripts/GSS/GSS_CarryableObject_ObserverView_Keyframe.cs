8ddba83a27a151f7d36638bc277a110d7291e83d
Xsear <Xsear@users.noreply.github.com>
2020-11-22 22:33:27 +0100

    Further carryable object keyframe/update

--- Scripts/GSS/GSS_CarryableObject_ObserverView_Keyframe.cs
+++ Scripts/GSS/GSS_CarryableObject_ObserverView_Keyframe.cs
@@ -10,17 +10,93 @@ namespace PacketPeepScript
         public byte Unk2;
         public float[] Position;
         public float[] MaybeRotation;
-        
+        public byte Unk3;
+        public byte Unk3_Extra;
+        public ushort[] Unk4_StatusEffectTimes;
+
+        public float Unk_Float0;
+        public float Unk_Float1;
+        public float Unk_Float2;
+        public float Unk_Float3;
+        public float Unk_Float4;
+        public float Unk_Float5;
+        public float Unk_Float6;
+        public float Unk_Float7;
+        public float Unk_Float8;
+        public float Unk_Float9;
+
+        public ushort? StatusEffect_00_ShortTime;
+        public uint? StatusEffect_00_Id;
+        public byte[] StatusEffect_00_Unk1;
+        public byte[] StatusEffect_00_Entity;
+        public uint? StatusEffect_00_Time1;
+        public byte? StatusEffect_00_MoreDataFlag;
+        public byte? StatusEffect_00_Data1_Count;
+        public byte[] StatusEffect_00_Data1_Entity;
+        public byte[] StatusEffect_00_UnkData;
+        public float? StatusEffect_00_Data_Float1;
+        public float? StatusEffect_00_Data_Float2;
+        public bool? StatusEffect_00_Cancel;
+
+        public byte[] MaybeFaction; // Total guess
+
+        public byte[] Unk6;
+
         public override void Read(Bitter.BinaryStream Stream)
         {
             Stream.ByteOrder = BinaryStream.Endianness.LittleEndian;
 
-            Unk1 = Stream.Read.ByteArray(8);
-            CarryableTypeId = Stream.Read.UInt();
-            Unk2 = Stream.Read.Byte();
+            if (true) {
+                Unk1 = Stream.Read.ByteArray(8);
+                CarryableTypeId = Stream.Read.UInt();
+                Unk2 = Stream.Read.Byte();
 
-            Position = Stream.Read.FloatArray(3);
-            MaybeRotation = Stream.Read.FloatArray(4);
+                Position = Stream.Read.FloatArray(3);
+                MaybeRotation = Stream.Read.FloatArray(4);
+
+                Unk3 = Stream.Read.Byte(); // Does this indicate additional status effects??
+                if (Unk3 > 0x00) {
+                    Unk3_Extra = Stream.Read.Byte();
+                }
+                Unk4_StatusEffectTimes = Stream.Read.UShortArray(12); // Probably just 12, unk3 throws it off though so went with 14
+
+                Unk_Float0 = Stream.Read.Float();
+                Unk_Float1 = Stream.Read.Float();
+                Unk_Float2 = Stream.Read.Float();
+                Unk_Float3 = Stream.Read.Float();
+                Unk_Float4 = Stream.Read.Float();
+                Unk_Float5 = Stream.Read.Float();
+                Unk_Float6 = Stream.Read.Float();
+                Unk_Float7 = Stream.Read.Float();
+                Unk_Float8 = Stream.Read.Float();
+                Unk_Float9 = Stream.Read.Float();
+
+
+
+
+                // Its a bit eeerily similar
+                StatusEffect_00_Id     = Stream.Read.UInt();
+                StatusEffect_00_Unk1   = Stream.Read.ByteArray(1);
+                StatusEffect_00_Entity = Stream.Read.ByteArray(8);
+                StatusEffect_00_Time1  = Stream.Read.UInt();
+                StatusEffect_00_MoreDataFlag = Stream.Read.Byte();
+                if (StatusEffect_00_MoreDataFlag == 0x01)
+                {
+                    StatusEffect_00_Data1_Count = Stream.Read.Byte();
+                    if (StatusEffect_00_Data1_Count > 0) {
+                        StatusEffect_00_Data1_Entity = Stream.Read.ByteArray(8*(int)StatusEffect_00_Data1_Count);
+                    }
+                    StatusEffect_00_UnkData   = Stream.Read.ByteArray(29);
+                    StatusEffect_00_Data_Float1 = Stream.Read.Float();
+                    StatusEffect_00_Data_Float2 = Stream.Read.Float();
+                }
+
+                MaybeFaction = Stream.Read.ByteArray(2);
+
+                Unk6 = Stream.Read.ByteArray(13);
+
+            }
+            
         }
     }
 }
--- Scripts/GSS/GSS_CarryableObject_ObserverView_Update.cs
+++ Scripts/GSS/GSS_CarryableObject_ObserverView_Update.cs
@@ -8,7 +8,14 @@ namespace PacketPeepScript
         {
             Position = 0x02,
             Unk_0x06 = 0x06,
+
+            StatusEffect_08_ShortTime = 0x08,
+            StatusEffect_08_Data = 0x28,
+            StatusEffect_08_Cancel = 0xa8,
+
             Unk_0x65 = 0x65,
+
+            Unk_0x86 = 0x86, // not sure if call with no data or if just a byte that belonged elsewhere
         }
 
         public float[] Position;
@@ -17,6 +24,21 @@ namespace PacketPeepScript
         public float[] Unk_0x65_Position;
         public float[] Unk_0x65_MaybeVelocity;
         public uint? Unk_0x65_Time;
+
+
+        public ushort? StatusEffect_08_ShortTime;
+        public uint? StatusEffect_08_Id; 
+        public byte[] StatusEffect_08_Unk1;
+        public byte[] StatusEffect_08_Entity;
+        public uint? StatusEffect_08_Time1;
+        public byte? StatusEffect_08_MoreDataFlag;
+        public byte? StatusEffect_08_Data1_Count;
+        public byte[] StatusEffect_08_Data1_Entity;
+        public byte[] StatusEffect_08_UnkData;
+        public float? StatusEffect_08_Data_Float1;
+        public float? StatusEffect_08_Data_Float2;
+        public bool? StatusEffect_08_Cancel;
+
 
         public byte[] UnableToParse;
 
@@ -40,6 +62,36 @@ namespace PacketPeepScript
                         Unk_0x65_Position = Stream.Read.FloatArray(3);
                         Unk_0x65_MaybeVelocity = Stream.Read.FloatArray(3);
                         Unk_0x65_Time = Stream.Read.UInt();
+                        break;
+
+                    case ShadowFieldIndex.StatusEffect_08_ShortTime:
+                        StatusEffect_08_ShortTime = Stream.Read.UShort();
+                        break;
+
+                    case ShadowFieldIndex.StatusEffect_08_Data:
+                        StatusEffect_08_Id     = Stream.Read.UInt();
+                        StatusEffect_08_Unk1   = Stream.Read.ByteArray(1);
+                        StatusEffect_08_Entity = Stream.Read.ByteArray(8);
+                        StatusEffect_08_Time1  = Stream.Read.UInt();
+                        StatusEffect_08_MoreDataFlag = Stream.Read.Byte();
+                        if (StatusEffect_08_MoreDataFlag == 0x01)
+                        {
+                            StatusEffect_08_Data1_Count = Stream.Read.Byte();
+                            if (StatusEffect_08_Data1_Count > 0) {
+                                StatusEffect_08_Data1_Entity = Stream.Read.ByteArray(8*(int)StatusEffect_08_Data1_Count);
+                            }
+                            StatusEffect_08_UnkData   = Stream.Read.ByteArray(29);
+                            StatusEffect_08_Data_Float1 = Stream.Read.Float();
+                            StatusEffect_08_Data_Float2 = Stream.Read.Float();
+                        }
+                        break;
+
+                    case ShadowFieldIndex.StatusEffect_08_Cancel:
+                        StatusEffect_08_Cancel = true;
+                        break;
+
+
+                    case ShadowFieldIndex.Unk_0x86:
                         break;
 
                     default:
