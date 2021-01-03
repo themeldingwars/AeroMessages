36f5258ab960ffa2eb7c78df951a0c4c616d8ef9
Xsear <Xsear@users.noreply.github.com>
2021-01-03 17:48:18 +0100

    Update Outpost ObserverView

--- Scripts/GSS/GSS_Outpost_ObserverView_Keyframe.cs
+++ Scripts/GSS/GSS_Outpost_ObserverView_Keyframe.cs
@@ -4,7 +4,7 @@ namespace PacketPeepScript
     [Script(MessageType.GSS, 45, 3, true)]
     public class OutpostObserverViewKeyframe : BaseScript
     {
-        public byte[] Unk1;
+        public byte[] Unk_Bitfield;
         public uint NameLocalizationId;
         public float[] Position;
         public uint LevelRangeId;
@@ -21,7 +21,8 @@ namespace PacketPeepScript
         public ushort MWMax;
         public uint MapMarkerTypeId;
         public float Radius;
-        public byte[] Unk3;
+        public byte[] Unk3_1;
+        public byte[] Unk3_2;
         public byte[] EncounterId;
         public byte[] Unk4;
 
@@ -29,26 +30,32 @@ namespace PacketPeepScript
         {
             Stream.ByteOrder = BinaryStream.Endianness.LittleEndian;
 
-            Unk1 = Stream.Read.ByteArray(2);
-            NameLocalizationId = Stream.Read.UInt();
-            Position = Stream.Read.FloatArray(3);
-            LevelRangeId = Stream.Read.UInt();
-            Synced = Stream.Read.Byte();
-            TeleportCost = Stream.Read.Int();
-            Progress = Stream.Read.Float();
-            FactionId = Stream.Read.Byte();
-            Team = Stream.Read.Byte();
-            UnderAttack = Stream.Read.Byte();
-            OutpostType = Stream.Read.Byte();
-            PossibleBuffsId = Stream.Read.UInt();
-            PowerLevel = Stream.Read.Byte();
-            MWCurrent = Stream.Read.UShort();
-            MWMax = Stream.Read.UShort();
-            MapMarkerTypeId = Stream.Read.UInt();
-            Radius = Stream.Read.Float(); 
-            Unk3 = Stream.Read.ByteArray(8);
-            EncounterId = Stream.Read.ByteArray(8);
-            Unk4 = Stream.Read.ByteArray(8);
+            if (true) {
+                Unk_Bitfield = Stream.Read.BitArray(16); 
+                NameLocalizationId = Stream.Read.UInt();
+                Position = Stream.Read.FloatArray(3);
+                LevelRangeId = Stream.Read.UInt();
+                Synced = Stream.Read.Byte();
+                TeleportCost = Stream.Read.Int();
+                Progress = Stream.Read.Float();
+                FactionId = Stream.Read.Byte();
+                Team = Stream.Read.Byte();
+                UnderAttack = Stream.Read.Byte();
+                OutpostType = Stream.Read.Byte();
+                PossibleBuffsId = Stream.Read.UInt();
+                PowerLevel = Stream.Read.Byte();
+                MWCurrent = Stream.Read.UShort();
+                MWMax = Stream.Read.UShort();
+                MapMarkerTypeId = Stream.Read.UInt();
+                Radius = Stream.Read.Float(); 
+                Unk3_1 = Stream.Read.ByteArray(4);
+                if (Unk_Bitfield[0] == 0x00) {
+                    Unk3_2 = Stream.Read.ByteArray(4);
+                }
+                EncounterId = Stream.Read.ByteArray(8);
+                Unk4 = Stream.Read.ByteArray(8);
+            }
+            
         }
     }
 }
