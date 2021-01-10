9006ffa67f2dec8d59c29c7f4ca54f2c691ca1f8
Xsear <Xsear@users.noreply.github.com>
2021-01-10 17:00:58 +0100

    Update outpost observer view shadowfields

--- Scripts/GSS/GSS_Outpost_ObserverView_Update.cs
+++ Scripts/GSS/GSS_Outpost_ObserverView_Update.cs
@@ -1,18 +1,52 @@
 using Bitter;
 namespace PacketPeepScript
 {
-    // This pretty rough, not tested
     [Script(MessageType.GSS, 45, 1, true)]
     public class OutpostObserverViewUpdate : BaseScript
     {
         enum ShadowFieldIndex : byte
         {
-            Unk_0x06 = 0x06,
-            Unk_0x08 = 0x08, 
+            OutpostName            = 0x00,
+            Position               = 0x01,
+            LevelBandId            = 0x02,
+            SinUnlockIndex         = 0x03,
+            TeleportCost           = 0x04,
+            Dynamic_00             = 0x05,
+            Dynamic_01             = 0x06,
+            Dynamic_02             = 0x07,
+            Dynamic_03             = 0x08,
+            Dynamic_04             = 0x09,
+            Dynamic_05             = 0x0a,
+            Dynamic_06             = 0x0b,
+            Dynamic_07             = 0x0c,
+            Dynamic_08             = 0x0d,
+            Dynamic_09             = 0x0e,
+            Dynamic_10             = 0x0f,
+            Dynamic_11             = 0x10,
+            NearbyResourceItems_0  = 0x11,
+            NearbyResourceItems_1  = 0x12,
+            NearbyResourceItems_2  = 0x13,
+            NearbyResourceItems_3  = 0x14,
+            NearbyResourceItems_4  = 0x15,
+            NearbyResourceItems_5  = 0x16,
+            NearbyResourceItems_6  = 0x17,
+            NearbyResourceItems_7  = 0x18,
+            NearbyResourceItems_8  = 0x19,
+            NearbyResourceItems_9  = 0x1a,
+            NearbyResourceItems_10 = 0x1b,
+            NearbyResourceItems_11 = 0x1c,
+            NearbyResourceItems_12 = 0x1d,
+            NearbyResourceItems_13 = 0x1e,
+            NearbyResourceItems_14 = 0x1f,
+            NearbyResourceItems_15 = 0x20,
+            EncounterId            = 0x21,
+            ScopeBubbleInfo        = 0x22,
         }
 
-        public byte? Unk_0x06;
-        public byte? Unk_0x08;
+        public string UnableToParseWarning; // Will be set if we encounter an unhandled shadowfield
+
+        public byte? Dynamic_01;
+        public byte? Dynamic_03;
 
         public byte[] UnableToParse;
 
@@ -25,14 +59,15 @@ namespace PacketPeepScript
                 ShadowFieldIndex sfidx = (ShadowFieldIndex) (Stream.Read.Byte());
                 switch (sfidx)
                 {
-                    case ShadowFieldIndex.Unk_0x06:
-                        Unk_0x06 = Stream.Read.Byte();
+                    case ShadowFieldIndex.Dynamic_01:
+                        Dynamic_01 = Stream.Read.Byte();
                         break;
-                    case ShadowFieldIndex.Unk_0x08:
-                        Unk_0x08 = Stream.Read.Byte();
+                    case ShadowFieldIndex.Dynamic_03:
+                        Dynamic_03 = Stream.Read.Byte();
                         break;
 
                     default:
+                        UnableToParseWarning = $"Dont know how to parse shadowfield {sfidx}";
                         int remaining = (int)(Stream.baseStream.Length - Stream.baseStream.ByteOffset);
                         UnableToParse = Stream.Read.ByteArray(remaining);
                         break;
