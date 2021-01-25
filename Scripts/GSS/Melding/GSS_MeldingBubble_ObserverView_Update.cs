c998234d7f24851feebc8b4ba3d56ad25848791d
Xsear <Xsear@users.noreply.github.com>
2021-01-25 22:07:39 +0100

    Add MeldingBubble ObserverView

--- Scripts/GSS/Melding/GSS_MeldingBubble_ObserverView_Update.cs
+++ Scripts/GSS/Melding/GSS_MeldingBubble_ObserverView_Update.cs
@@ -15,6 +15,14 @@ namespace PacketPeepScript
 
         public string UnableToParseWarning; // Will be set if we encounter an unhandled shadowfield
 
+        public float[] Position;
+        public uint? Position_Time;
+        public float Radius;
+        public uint Radius_Time;
+        public byte BubbleType;
+        public byte FxFlags;
+        public byte[] ScopeBubbleInfo;
+
         public byte[] UnableToParse;
 
         public override void Read(Bitter.BinaryStream Stream)
@@ -26,6 +34,27 @@ namespace PacketPeepScript
                 ShadowFieldIndex sfidx = (ShadowFieldIndex) (Stream.Read.Byte());
                 switch (sfidx)
                 {
+                    case ShadowFieldIndex.Position:
+                        Position = Stream.Read.FloatArray(3);
+                        Position_Time = Stream.Read.UInt();
+                        break;
+
+                    case ShadowFieldIndex.Radius:
+                        Radius = Stream.Read.Float();
+                        Radius_Time = Stream.Read.UInt();
+                        break;
+
+                    case ShadowFieldIndex.BubbleType:
+                        BubbleType = Stream.Read.Byte();
+                        break;
+                    case ShadowFieldIndex.FxFlags:
+                        FxFlags = Stream.Read.Byte();
+                        break;
+
+                    case ShadowFieldIndex.ScopeBubbleInfo:
+                        ScopeBubbleInfo = Stream.Read.ByteArray(8);
+                        break;
+
                     default:
                         UnableToParseWarning = $"Dont know how to parse shadowfield {sfidx}";
                         int remaining = (int)(Stream.baseStream.Length - Stream.baseStream.ByteOffset);
