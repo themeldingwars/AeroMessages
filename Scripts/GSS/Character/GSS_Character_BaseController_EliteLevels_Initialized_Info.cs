c37fbe53a7472d1cb919b6068a5dc67dc21311d7
Xsear <Xsear@users.noreply.github.com>
2021-02-08 18:04:50 +0100

    Name the vars in EliteLevels InitInfo

--- Scripts/GSS/Character/GSS_Character_BaseController_EliteLevels_Initialized_Info.cs
+++ Scripts/GSS/Character/GSS_Character_BaseController_EliteLevels_Initialized_Info.cs
@@ -4,20 +4,19 @@ namespace PacketPeepScript
     [Script(MessageType.GSS, 2, 193, true)]
     public class CharacterBaseControllerEliteLevelsInitializedInfo : BaseScript
     {
-        // need to verify
-        public uint Var1; // award_frame_min_level
-        public uint Var2; // levels_per_rare
-        public uint Var3; // revert_cost
-        public uint Var4; // reroll_cost
+        public uint LevelsPerRare; // award_frame_min_level
+        public uint AwardFrameMinLevel; // levels_per_rare
+        public uint RevertCost; // revert_cost
+        public uint RerollCost; // reroll_cost
 
         public override void Read(Bitter.BinaryStream Stream)
         {
             Stream.ByteOrder = BinaryStream.Endianness.LittleEndian;
 
-            Var1 = Stream.Read.UInt();
-            Var2 = Stream.Read.UInt();
-            Var3 = Stream.Read.UInt();
-            Var4 = Stream.Read.UInt();
+            LevelsPerRare = Stream.Read.UInt();
+            AwardFrameMinLevel = Stream.Read.UInt();
+            RevertCost = Stream.Read.UInt();
+            RerollCost = Stream.Read.UInt();
         }
     }
 }
