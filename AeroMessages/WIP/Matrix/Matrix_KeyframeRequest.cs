3c4ccf49b90ddeea9de4534605d2f74ef6d25a41
Xsear <Xsear@users.noreply.github.com>
2021-05-29 21:00:12 +0200

    Adjust

--- AeroMessages/WIP/Matrix/Matrix_KeyframeRequest.cs
+++ AeroMessages/WIP/Matrix/Matrix_KeyframeRequest.cs
@@ -10,6 +10,21 @@ using System.Runtime.InteropServices;
 using Aero.Gen.Attributes;
 namespace AeroMessages.Matrix
 {
+    [Aero]
+    public partial class Matrix_KeyframeRequest
+    {
+        public byte HaveRequestByEntityID;
+        [AeroIf(nameof(HaveRequestByEntityID), 1)]
+        [AeroArray(typeof(byte))]
+        public RequestByEntity[] EntityRequests;
+
+        public byte HaveRequestByRefID;
+        [AeroIf(nameof(HaveRequestByRefID), 1)]
+        [AeroArray(typeof(byte))]
+        public ushort[] RefRequests;
+
+    }
+
     [AeroBlock]
     public struct RequestByEntity
     {
@@ -17,20 +32,5 @@ namespace AeroMessages.Matrix
         public ushort RefID;
         public byte Unk2;
         public uint Checksum;
-    }
-
-    [Aero]
-    public partial class Matrix_KeyframeRequest
-    {
-        public byte HaveRequestByEntityID;
-        //[AeroIf(nameof(HaveRequestByEntityID), 1)]
-        //[AeroArray(typeof(byte))]
-        //public RequestByEntity[] EntityRequests; // TODO: FIXME: Gen issue
-
-        public byte HaveRequestByRefID;
-        [AeroIf(nameof(HaveRequestByRefID), 1)]
-        [AeroArray(typeof(byte))]
-        public ushort[] RefRequests;
-
     }
 }
