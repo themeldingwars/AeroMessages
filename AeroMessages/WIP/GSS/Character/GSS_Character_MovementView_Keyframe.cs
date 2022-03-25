edceec3b21c6de7bd3cf854277875122097cf1e2
Xsear <Xsear@users.noreply.github.com>
2022-03-25 17:10:25 +0100

    Convert Character Views

--- AeroMessages/WIP/GSS/Character/GSS_Character_MovementView_Keyframe.cs
+++ AeroMessages/WIP/GSS/Character/GSS_Character_MovementView_Keyframe.cs
@@ -1,13 +1,3 @@
-/*
-META_BEGIN
-    MsgType: GSS
-    FromServer: True
-    TypeCode: 12
-    TypeName: Character::MovementView
-    MsgId: 3
-    MsgName: Keyframe
-META_END
- */
 using Aero.Gen.Attributes;
 using AeroMessages.Common;
 using System.Numerics;
