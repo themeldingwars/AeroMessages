38f929ac4566d25ad1e252cb414423fb8cfedb32
Xsear <Xsear@users.noreply.github.com>
2022-03-25 16:52:51 +0100

    Convert AreaVisualData

---
+++ AeroMessages/WIP/GSS/AreaVisualData/GSS_AreaVisualData_MapMarkerView.cs
@@ -0,0 +1,71 @@
+using Aero.Gen.Attributes;
+using static Aero.Gen.Attributes.AeroIfAttribute;
+using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+using AeroMessages.Common;
+using System.Numerics;
+using System;
+namespace AeroMessages.GSS.AreaVisualData
+{
+    [Aero(true)]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 22, 1)]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 22, 3)]
+    public partial class AreaVisualData_MapMarkerView
+    {
+        private Vector3 Position;
+
+        [AeroNullable] private MapMarkerData MapMarkers_0;
+        [AeroNullable] private MapMarkerData MapMarkers_1;
+        [AeroNullable] private MapMarkerData MapMarkers_2;
+        [AeroNullable] private MapMarkerData MapMarkers_3;
+        [AeroNullable] private MapMarkerData MapMarkers_4;
+        [AeroNullable] private MapMarkerData MapMarkers_5;
+        [AeroNullable] private MapMarkerData MapMarkers_6;
+        [AeroNullable] private MapMarkerData MapMarkers_7;
+        [AeroNullable] private MapMarkerData MapMarkers_8;
+        [AeroNullable] private MapMarkerData MapMarkers_9;
+        [AeroNullable] private MapMarkerData MapMarkers_10;
+        [AeroNullable] private MapMarkerData MapMarkers_11;
+        [AeroNullable] private MapMarkerData MapMarkers_12;
+        [AeroNullable] private MapMarkerData MapMarkers_13;
+        [AeroNullable] private MapMarkerData MapMarkers_14;
+        [AeroNullable] private MapMarkerData MapMarkers_15;
+        [AeroNullable] private MapMarkerData MapMarkers_16;
+        [AeroNullable] private MapMarkerData MapMarkers_17;
+        [AeroNullable] private MapMarkerData MapMarkers_18;
+        [AeroNullable] private MapMarkerData MapMarkers_19;
+        [AeroNullable] private MapMarkerData MapMarkers_20;
+        [AeroNullable] private MapMarkerData MapMarkers_21;
+        [AeroNullable] private MapMarkerData MapMarkers_22;
+        [AeroNullable] private MapMarkerData MapMarkers_23;
+        [AeroNullable] private MapMarkerData MapMarkers_24;
+        [AeroNullable] private MapMarkerData MapMarkers_25;
+        [AeroNullable] private MapMarkerData MapMarkers_26;
+        [AeroNullable] private MapMarkerData MapMarkers_27;
+        [AeroNullable] private MapMarkerData MapMarkers_28;
+        [AeroNullable] private MapMarkerData MapMarkers_29;
+        [AeroNullable] private MapMarkerData MapMarkers_30;
+        [AeroNullable] private MapMarkerData MapMarkers_31;
+        [AeroNullable] private MapMarkerData MapMarkers_32;
+        [AeroNullable] private MapMarkerData MapMarkers_33;
+        [AeroNullable] private MapMarkerData MapMarkers_34;
+    }
+
+    [AeroBlock]
+    public struct MapMarkerData
+    {
+        [AeroSdb("dbencounterdata::MapMarkerInfo", "id")]
+        public uint MarkerType;
+
+        [AeroArray(6)]
+        public byte[] Unk3;
+
+        public EntityId EncounterId;
+        public EntityId EncounterMarkerId; // No controller specified.
+
+        public byte HasDuration;
+        [AeroIf(nameof(HasDuration), 1)]
+        public uint ExpireAtTime; // Gametime ms
+
+        public HalfVector3 Position;
+    }
+}
---
+++ AeroMessages/WIP/GSS/AreaVisualData/GSS_AreaVisualData_TinyObjectView.cs
@@ -0,0 +1,47 @@
+using Aero.Gen.Attributes;
+using static Aero.Gen.Attributes.AeroIfAttribute;
+using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+using AeroMessages.Common;
+using System.Numerics;
+using System;
+namespace AeroMessages.GSS.AreaVisualData
+{
+    [Aero(true)]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 23, 1)]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 23, 3)]
+    public partial class AreaVisualData_TinyObjectView
+    {
+        [AeroNullable] private TinyObjectData TinyObjects_0;
+        [AeroNullable] private TinyObjectData TinyObjects_1;
+        [AeroNullable] private TinyObjectData TinyObjects_2;
+        [AeroNullable] private TinyObjectData TinyObjects_3;
+        [AeroNullable] private TinyObjectData TinyObjects_4;
+        [AeroNullable] private TinyObjectData TinyObjects_5;
+        [AeroNullable] private TinyObjectData TinyObjects_6;
+        [AeroNullable] private TinyObjectData TinyObjects_7;
+        [AeroNullable] private TinyObjectData TinyObjects_8;
+        [AeroNullable] private TinyObjectData TinyObjects_9;
+        [AeroNullable] private TinyObjectData TinyObjects_10;
+        [AeroNullable] private TinyObjectData TinyObjects_11;
+        [AeroNullable] private TinyObjectData TinyObjects_12;
+        [AeroNullable] private TinyObjectData TinyObjects_13;
+        [AeroNullable] private TinyObjectData TinyObjects_14;
+        [AeroNullable] private TinyObjectData TinyObjects_15;
+        [AeroNullable] private TinyObjectData TinyObjects_16;
+        [AeroNullable] private TinyObjectData TinyObjects_17;
+        [AeroNullable] private TinyObjectData TinyObjects_18;
+        [AeroNullable] private TinyObjectData TinyObjects_19;
+        [AeroNullable] private TinyObjectData TinyObjects_20;
+        [AeroNullable] private TinyObjectData TinyObjects_21;
+        [AeroNullable] private TinyObjectData TinyObjects_22;
+        [AeroNullable] private TinyObjectData TinyObjects_23;
+        [AeroNullable] private TinyObjectData TinyObjects_24;
+        [AeroNullable] private TinyObjectData TinyObjects_25;
+        [AeroNullable] private TinyObjectData TinyObjects_26;
+        [AeroNullable] private TinyObjectData TinyObjects_27;
+        [AeroNullable] private TinyObjectData TinyObjects_28;
+        [AeroNullable] private TinyObjectData TinyObjects_29;
+        [AeroNullable] private TinyObjectData TinyObjects_30;
+        [AeroNullable] private TinyObjectData TinyObjects_31;
+    }
+}
