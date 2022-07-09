218b3cabbb8090e7225d2cf473eed73b53167393
Xsear <Xsear@users.noreply.github.com>
2022-07-09 16:09:10 +0200

    Add missing view tag for movement

--- AeroMessages/GSS/V66/Character/View/MovementView.cs
+++ AeroMessages/GSS/V66/Character/View/MovementView.cs
@@ -1,10 +1,11 @@
 using Aero.Gen.Attributes;
 using AeroMessages.Common;
 using System.Numerics;
+using Aero.Gen;
 using static Aero.Gen.Attributes.AeroMessageIdAttribute;
 namespace AeroMessages.GSS.V66.Character.View
 {
-    [Aero]
+    [Aero(AeroGenTypes.View)]
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 12, 1)]
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 12, 3)]
     public partial class MovementView
