f0a0c430131b01546d826edd55f7747d0a57161c
Xsear <Xsear@users.noreply.github.com>
2022-07-27 16:30:34 +0200

    Add Matrix ExitZone, ExitZoneAck (no data messages)

---
+++ AeroMessages/Matrix/V25/ExitZoneAck.cs
@@ -0,0 +1,13 @@
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Aero.Gen.Attributes;
+using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+namespace AeroMessages.Matrix.V25
+{
+    [Aero]
+    [AeroMessageId(MsgType.Matrix, MsgSrc.Command, 19)]
+    public partial class ExitZoneAck
+    {
+        // Nothing to parse
+    }
+}
