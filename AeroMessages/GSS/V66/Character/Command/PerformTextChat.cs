fd1ba512bb49c38c2341b7ecd02cdde650a475ec
Xsear <Xsear@users.noreply.github.com>
2022-07-28 20:14:16 +0200

    Update Character PerformTextChat

--- AeroMessages/GSS/V66/Character/Command/PerformTextChat.cs
+++ AeroMessages/GSS/V66/Character/Command/PerformTextChat.cs
@@ -1,18 +1,38 @@
 using Aero.Gen.Attributes;
 using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+using System.Numerics;
+using static Aero.Gen.Attributes.AeroIfAttribute;
 namespace AeroMessages.GSS.V66.Character.Command
 {
     [Aero]
     [AeroMessageId(MsgType.GSS, MsgSrc.Command, 2, 145)]
     public partial class PerformTextChat
     {
-        [AeroString]
-        public string Message;
-        public byte Channel; // Might be a ushort
+        [AeroString] public string Message;
+        public byte Channel;
 
-        // Remaining bytes are used in whispers (8) and friends chat (14)
-        // Not sure about their contents.
-        //[AeroArray(-1)]
-        //public byte[] Unk;
+        // 009fe600
+        public byte HaveUnk1;
+        [AeroIf(nameof(HaveUnk1), 1)]
+        public ulong Unk1;
+
+        // 009e5820
+        public byte HaveUnk2;
+        [AeroIf(nameof(HaveUnk2), 1)]
+        [AeroString] public string Unk2;
+
+        public byte Unk3;
+
+        // 009f25a0
+        public byte HaveUnk4;
+        [AeroIf(nameof(HaveUnk4), 1)]
+        public ChatUnk4Data Unk4;
+    }
+
+    [AeroBlock]
+    public struct ChatUnk4Data
+    {
+        [AeroArray(typeof(ushort))]
+        public byte[] Data;
     }
 }
