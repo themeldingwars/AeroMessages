56a0b9ecf2151f6f92c4e2edfa2c72be78f62c4a
Xsear <Xsear@users.noreply.github.com>
2022-07-30 10:09:50 +0200

    Add Character RunTeamManagerCommand

---
+++ AeroMessages/GSS/V66/Character/Command/RunTeamManagerCommand.cs
@@ -0,0 +1,22 @@
+using Aero.Gen.Attributes;
+using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+namespace AeroMessages.GSS.V66.Character.Command
+{
+    [Aero]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Command, 2, 232)]
+    public partial class RunTeamManagerCommand
+    {
+        public enum TeamManagerCommandType : byte
+        {
+            Invite = 0,
+            Accept = 1,
+            Decline = 2,
+            Change = 3,
+            Create = 4,
+            Kick = 5
+        }
+        public TeamManagerCommandType Type;
+        public string Unk1;
+        public byte Unk2;
+    }
+}
