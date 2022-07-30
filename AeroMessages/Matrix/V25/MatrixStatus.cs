a8a5df6cbbfe90a64e0332e11cd185e9ec79324e
Xsear <Xsear@users.noreply.github.com>
2022-07-30 16:48:27 +0200

    Update Matrix MatrixStatus

--- AeroMessages/Matrix/V25/MatrixStatus.cs
+++ AeroMessages/Matrix/V25/MatrixStatus.cs
@@ -14,7 +14,23 @@ namespace AeroMessages.Matrix.V25
         public byte Unk4;
         public ushort Unk5;
         public byte Unk6;
-        public byte Unk7; // Something different
-        public ushort Unk8;
+
+        // -- FUN_00754d40
+        public byte HaveUnk7;
+        [AeroIf(nameof(HaveUnk7), 1)]
+        [AeroArray(typeof(byte))] public MatrixStatusUnk7Data[] Unk7;
+        // --
+
+        [AeroArray(typeof(ushort))] public byte[] Unk8; // FUN_016eed70, not 100%
+    }
+    
+    [AeroBlock]
+    public struct MatrixStatusUnk7Data
+    {
+        [AeroArray(8)] public byte[] Unk1;
+        public byte Unk2;
+        public byte Unk3;
+        public byte Unk4;
+        public byte Unk5;
     }
 }
