b50cabf0d352b3127d37cab9365b2ee5d737b9e3
Xsear <Xsear@users.noreply.github.com>
2022-07-30 12:08:24 +0200

    Update Matrix EnterZone

--- AeroMessages/Matrix/V25/EnterZone.cs
+++ AeroMessages/Matrix/V25/EnterZone.cs
@@ -7,42 +7,63 @@ namespace AeroMessages.Matrix.V25
     [AeroMessageId(MsgType.Matrix, MsgSrc.Message, 37)]
     public partial class EnterZone
     {
+        // -- FUN_00757370
         public ulong InstanceId;
         public uint ZoneId;
         public ulong ZoneTimestamp;
         public byte PreviewModeFlag;
-
-        [AeroString]
-        public string ZoneOwner;
-
-        [AeroArray(6)]
-        public byte[] Unk1;
-
+        [AeroString] public string ZoneOwner;
+        public ushort Unk1_1; // Looks like Build Number
+        public uint Unk1_2; // Looks like Build Source Revision
         public byte HotfixLevel;
         public ulong MatchId;
-
-        [AeroArray(1)]
-        public byte[] Unk2;
+        public byte Unk2;
         public uint Unk3_Millis;
+        [AeroString] public string ZoneName;
 
-        [AeroString]
-        public string ZoneName;
+        public byte HaveUnk4; // Seems to be a big thing when not 0
+        [AeroIf(nameof(HaveUnk4), 1)]
+        public EZU4Data Unk4;
+        // --
 
-        public byte Unk4;
+        public ulong ZoneTime_Unk1;
+        public float ZoneTime_Unk2;
+        public float ZoneTime_Unk3;
 
-        [AeroArray(16)]
-        public byte[] Unk_ZoneTime;
-
+        // -- FUN_00757770
         public ulong Unk5_MicroUnix;
         public ulong Unk6_MicroUnix;
-
-        [AeroArray(4)]
-        public byte[] Unk7;
-        public float Timescale;
-
-        [AeroArray(17)]
-        public byte[] Unk8;
+        [AeroArray(4)] public byte[] Timescale_1;
+        public float Timescale_2;
+        public ulong Unk7;
+        public ulong Unk8;
+        public byte Unk9;
+        // --
 
         public byte SpectatorModeFlag;
+    }
+
+    [AeroBlock]
+    public struct EZU4Data
+    {
+        // FUN_00755d40
+        [AeroArray(typeof(byte))] EZU4Data_00755d40 Unk1;
+
+        // FUN_00756a50
+        [AeroArray(typeof(byte))] EZU4Data_00756a50 Unk2;
+    }   
+
+    [AeroBlock]
+    public struct EZU4Data_00755d40
+    {
+        [AeroString] public string A1;
+        public ushort A2;
+    }
+
+    [AeroBlock]
+    public struct EZU4Data_00756a50
+    {
+        [AeroString] public string B1;
+        public uint B2;
     }
 }
