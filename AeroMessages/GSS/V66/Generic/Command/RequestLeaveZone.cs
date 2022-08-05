8072efe3d4d0581426ba87b5bdb5ed7eab15eef7
Xsear <Xsear@users.noreply.github.com>
2022-08-05 13:04:08 +0200

    Add nodata generic commands

---
+++ AeroMessages/GSS/V66/Generic/Command/RequestLeaveZone.cs
@@ -0,0 +1,12 @@
+using Aero.Gen.Attributes;
+using System.Numerics;
+using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+namespace AeroMessages.GSS.V66.Generic
+{
+    [Aero]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Command, 251, 23)]
+    public partial class RequestLeaveZone
+    {
+        // Nothing to parse
+    }
+}
