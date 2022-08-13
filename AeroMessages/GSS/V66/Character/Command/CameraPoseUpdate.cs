07dd13c5386be7d169d61890cf11e3fe2e665ece
Xsear <Xsear@users.noreply.github.com>
2022-08-13 17:47:45 +0200

    Fix and update CameraPoseUpdate

--- AeroMessages/GSS/V66/Character/Command/CameraPoseUpdate.cs
+++ AeroMessages/GSS/V66/Character/Command/CameraPoseUpdate.cs
@@ -1,13 +1,21 @@
 using Aero.Gen.Attributes;
 using static Aero.Gen.Attributes.AeroMessageIdAttribute;
 using AeroMessages.Common;
+using System;
+using System.Numerics;
 namespace AeroMessages.GSS.V66.Character.Command
 {
     [Aero]
-    [AeroMessageId(MsgType.GSS, MsgSrc.Command, 2, 117)]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Command, 2, 177)]
     public partial class CameraPoseUpdate
     {
-        public byte Unk1;
+        public byte HaveEntity;
+        [AeroIf(nameof(HaveEntity), 1)] // Should be != 0
         public EntityId FollowingEntity;
+
+        // Assuming its position
+        public byte HavePosition;
+        [AeroIf(nameof(HavePosition), 1)] // Should be != 0
+        public Vector3 Position;
     }
 }
