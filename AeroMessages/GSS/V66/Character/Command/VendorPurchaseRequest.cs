e7f736228a8c015f7428109d4dada1c9edbc402f
Xsear <Xsear@users.noreply.github.com>
2022-08-13 16:19:43 +0200

    Update VendorPurchaseRequest

--- AeroMessages/GSS/V66/Character/Command/VendorPurchaseRequest.cs
+++ AeroMessages/GSS/V66/Character/Command/VendorPurchaseRequest.cs
@@ -9,19 +9,20 @@ namespace AeroMessages.GSS.V66.Character.Command
     public partial class VendorPurchaseRequest
     {
         public ulong Unk1;
-
         public EntityId Buyer;
         public ulong ProductID;
         public ulong PriceID;
 
-        public byte Unk2;
-
+        public byte HaveUnk2;
+        [AeroIf(nameof(HaveUnk2), 0x01)] // FIXME: The check should be != 0
         public uint ScuffedVendorID; // Half of the ulong...?
 
-        public byte Unk3;
-
+        public byte HaveUnk3;
+        [AeroIf(nameof(HaveUnk3), 0x01)] // FIXME: The check should be != 0
         public uint VendorRemoteID;
 
-        public byte Unk4;
+        public byte HaveUnk4;
+        [AeroIf(nameof(HaveUnk4), 0x01)] // FIXME: The check should be != 0
+        [AeroString] public string Unk4;
     }
 }
