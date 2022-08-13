a8779b640ff8780306a689947a6add9f6e666f7a
Xsear <Xsear@users.noreply.github.com>
2022-08-13 19:30:00 +0200

    Update DuelRequest

--- AeroMessages/GSS/V66/Character/Command/DuelRequest.cs
+++ AeroMessages/GSS/V66/Character/Command/DuelRequest.cs
@@ -4,23 +4,24 @@ using AeroMessages.Common;
 namespace AeroMessages.GSS.V66.Character.Command
 {
     [Aero]
-    [AeroMessageId(MsgType.GSS, MsgSrc.Command, 2, 118)]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Command, 2, 188)]
     public partial class DuelRequest
     {
-        [AeroArray(2)]
-        public byte[] Unk1;
-
+        // FUN_009db930
+        public byte Unk1;
+        [AeroString] public string Unk2;
         public EntityId RequestingEntity;
-
-        [AeroArray(9)]
-        public byte[] Unk2;
-
-        [AeroString]
-        public string TargetName;
-
-        [AeroArray(20)]
-        public byte[] Unk3;
-
-        public EntityId TargetEntity; // TODO: Verify this, might be in the Unk3
+        [AeroString] public string Unk3;
+        public EntityId Unk4;
+        [AeroString] public string TargetName;
+        [AeroArray(typeof(byte))] public ulong[] Unk5;
+        [AeroArray(typeof(byte))] public ulong[] Unk6;
+        public ushort Unk7;
+        public ushort Unk8;
+        public uint Unk9;
+        public byte Unk10;
+        public ulong TargetEntity; // TODO: Verify this
+        // --
+        public byte Unk12;
     }
 }
