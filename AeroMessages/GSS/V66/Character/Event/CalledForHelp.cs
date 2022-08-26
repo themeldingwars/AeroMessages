4350fb7d494c1325abe3f57bb55f6f9ec909a2f8
Xsear <Xsear@users.noreply.github.com>
2022-08-26 23:48:29 +0200

    Fix CalledForHelp wrong type

--- AeroMessages/GSS/V66/Character/Event/CalledForHelp.cs
+++ AeroMessages/GSS/V66/Character/Event/CalledForHelp.cs
@@ -3,7 +3,7 @@ using static Aero.Gen.Attributes.AeroMessageIdAttribute;
 namespace AeroMessages.GSS.V66.Character.Event
 {
     [Aero]
-    [AeroMessageId(MsgType.GSS, MsgSrc.Command, 8, 104)]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 8, 104)]
     public partial class CalledForHelp
     {
         public ushort ShortTime;
