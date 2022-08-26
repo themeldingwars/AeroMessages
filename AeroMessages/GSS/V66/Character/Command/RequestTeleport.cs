c00d8261a2dfb8bb0e9535cd81bb73fb2afab73a
Xsear <Xsear@users.noreply.github.com>
2022-08-26 21:47:00 +0200

    Update RequestTeleport

--- AeroMessages/GSS/V66/Character/Command/RequestTeleport.cs
+++ AeroMessages/GSS/V66/Character/Command/RequestTeleport.cs
@@ -6,7 +6,7 @@ namespace AeroMessages.GSS.V66.Character.Command
     [AeroMessageId(MsgType.GSS, MsgSrc.Command, 2, 210)]
     public partial class RequestTeleport
     {
-        public uint PosX;
-        public uint PosY;
+        public float PosX;
+        public float PosY;
     }
 }
