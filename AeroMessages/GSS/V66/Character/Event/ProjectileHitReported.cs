3b4cb667b49a376ea10f56dcb0ece1c2b7ec348f
Xsear <Xsear@users.noreply.github.com>
2022-09-03 15:02:06 +0200

    Update ProjectileHitReported

--- AeroMessages/GSS/V66/Character/Event/ProjectileHitReported.cs
+++ AeroMessages/GSS/V66/Character/Event/ProjectileHitReported.cs
@@ -3,13 +3,13 @@ using static Aero.Gen.Attributes.AeroMessageIdAttribute;
 namespace AeroMessages.GSS.V66.Character.Event
 {
     [Aero]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 5, 97)]
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 11, 97)]
-    public partial class ProjectileHitReported_View
+    public partial class ProjectileHitReported
     {
-        // TODO: Verify if same as controller or not
         public ushort Unk1; // Matches Unk1 in ReportProjectileHit?
         public ushort ShortTime; 
         public byte Unk2;
-        public byte Unk3_Always1;
+        public byte Unk3;
     }
 }
