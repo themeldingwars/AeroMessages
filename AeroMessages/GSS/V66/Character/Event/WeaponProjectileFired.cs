c2bc8452087716fa5bef0aab69436bd27a5735e5
Xsear <Xsear@users.noreply.github.com>
2022-09-03 17:12:52 +0200

    Update WeaponProjectileFired

--- AeroMessages/GSS/V66/Character/Event/WeaponProjectileFired.cs
+++ AeroMessages/GSS/V66/Character/Event/WeaponProjectileFired.cs
@@ -4,11 +4,14 @@ using static Aero.Gen.Attributes.AeroMessageIdAttribute;
 namespace AeroMessages.GSS.V66.Character.Event
 {
     [Aero]
+    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 5, 95)]
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 11, 95)]
-    public partial class WeaponProjectileFired_View
+    public partial class WeaponProjectileFired
     {
         public ushort ShortTime;
         public QuantisedVector3 Aim;
-        public byte Unk;
+        public byte HaveMoreData;
+        [AeroIf(nameof(HaveMoreData), 1)]
+        public HalfVector3 MoreData; // Not sure, 3x2
     }
 }
