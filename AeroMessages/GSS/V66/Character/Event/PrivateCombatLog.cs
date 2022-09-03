95faf44eff146418139b7b25b5be68752ce80651
Xsear <Xsear@users.noreply.github.com>
2022-09-03 15:01:07 +0200

    Update PrivateCombatLog

--- AeroMessages/GSS/V66/Character/Event/PrivateCombatLog.cs
+++ AeroMessages/GSS/V66/Character/Event/PrivateCombatLog.cs
@@ -6,9 +6,9 @@ namespace AeroMessages.GSS.V66.Character.Event
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 2, 92)]
     public partial class PrivateCombatLog
     {
-        [AeroArray(4)]
-        public byte[] Unk1;
-
+        // Yeah, this is how the parser reads it...
+        public int HaveData;
+        [AeroIf(nameof(HaveData), 1)]
         [AeroArray(typeof(ushort))]
         public byte[] Data; // Multiple variations, often gametime towards the end of a row.
     }
