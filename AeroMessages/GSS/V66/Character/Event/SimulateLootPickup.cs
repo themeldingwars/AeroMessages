1c56682e2997cc187f2241891390076f086d3fae
Xsear <Xsear@users.noreply.github.com>
2022-09-03 17:20:00 +0200

    Update SimulateLootPickup

--- AeroMessages/GSS/V66/Character/Event/SimulateLootPickup.cs
+++ AeroMessages/GSS/V66/Character/Event/SimulateLootPickup.cs
@@ -6,10 +6,23 @@ namespace AeroMessages.GSS.V66.Character.Event
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 2, 132)]
     public partial class SimulateLootPickup
     {
+        public LootItem Item;
+        public byte Unk1;
+        public byte HaveUnk2;
+        [AeroIf(nameof(HaveUnk2), 1)]
+        public ulong Unk2;
+    }
+
+    [AeroBlock]
+    public struct LootItem
+    {
         [AeroSdb("dbitems::RootItem", "sdb_id")]
         public uint LootSdbID;
-
-        //[AeroArray(-1)]
-        //public byte[] Unk;
+        public ushort Unk1;
+        public ushort Unk2;
+        public ushort Unk3;
+        public uint Unk4;
+        public uint Unk5;
+        public byte Unk6;
     }
 }
