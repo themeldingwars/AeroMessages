2e8334fa8818fe512c016752a4f323ece537f288
Xsear <Xsear@users.noreply.github.com>
2022-09-16 23:32:58 +0200

    Update LFGMatchFound

--- AeroMessages/Matrix/V25/LFGMatchFound.cs
+++ AeroMessages/Matrix/V25/LFGMatchFound.cs
@@ -2,16 +2,17 @@ using System.Numerics;
 using System.Runtime.InteropServices;
 using Aero.Gen.Attributes;
 using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+using AeroMessages.Common;
 namespace AeroMessages.Matrix.V25
 {
     [Aero]
     [AeroMessageId(MsgType.Matrix, MsgSrc.Message, 65)]
     public partial class LFGMatchFound
     {
-        public ulong Unk1;
-        public uint Unk2;
-        public ulong Unk3;
-        [AeroString] public string Unk4;
+        public ulong MatchId;
+        public uint LFGCategoryId;
+        public EntityId LeaderId;
+        [AeroString] public string LeaderName;
 
         [AeroArray(typeof(byte))]
         [AeroString] public string[] Unk5;
