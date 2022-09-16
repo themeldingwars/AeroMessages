372a84809b9555dfede3e54dce6fb5278a505222
Xsear <Xsear@users.noreply.github.com>
2022-09-16 23:32:36 +0200

    Update ClientPreferences

--- AeroMessages/Matrix/V25/ClientPreferences.cs
+++ AeroMessages/Matrix/V25/ClientPreferences.cs
@@ -3,12 +3,12 @@ using static Aero.Gen.Attributes.AeroMessageIdAttribute;
 namespace AeroMessages.Matrix.V25
 {
     [Aero]
-    [AeroMessageId(MsgType.Matrix, MsgSrc.Message, 26)]
+    [AeroMessageId(MsgType.Matrix, MsgSrc.Command, 26)]
     public partial class ClientPreferences
     {
-        public uint Unk1;
-        public uint Unk2;
-        public ushort Unk3;
-        public byte Unk4; // When debuglag.matrixDev is set to 1, Unk4 == 2. Otherwise == 0.
+        public uint MaxBytesPerSecond; // network.maxKBitPerSecond, value is sent as bytes rather than bits
+        public uint MinBatchDelay; // network.minBatchDelay
+        public ushort TargetMTU; // network.targetMTU, value sent caps at 1168 which is equiv to setting 1492 in the console.
+        public byte MatrixDebugFlags; // When debuglag.matrixDev is set to 1, MatrixDebugFlags -> 2. Otherwise -> 0.
     }
 }
