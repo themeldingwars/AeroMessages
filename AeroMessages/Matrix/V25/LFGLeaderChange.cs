06136b74632409dbd86b34d0610a2cab0454b069
Xsear <Xsear@users.noreply.github.com>
2022-09-16 23:32:48 +0200

    Update LFGLeaderChange

--- AeroMessages/Matrix/V25/LFGLeaderChange.cs
+++ AeroMessages/Matrix/V25/LFGLeaderChange.cs
@@ -2,14 +2,15 @@ using System.Numerics;
 using System.Runtime.InteropServices;
 using Aero.Gen.Attributes;
 using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+using AeroMessages.Common;
 namespace AeroMessages.Matrix.V25
 {
     [Aero]
     [AeroMessageId(MsgType.Matrix, MsgSrc.Message, 66)]
     public partial class LFGLeaderChange
     {
-        public ulong Unk1;
-        public ulong Unk2;
-        [AeroString] public string Unk3;
+        public ulong MatchId;
+        public EntityId LeaderId;
+        [AeroString] public string LeaderName;
     }
 }
