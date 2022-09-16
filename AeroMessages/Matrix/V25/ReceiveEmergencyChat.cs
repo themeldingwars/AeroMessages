eed5a86edf661327ba21d90982a8ef73346e4f45
Xsear <Xsear@users.noreply.github.com>
2022-09-16 23:33:10 +0200

    Update ReceiveEmergencyChat

--- AeroMessages/Matrix/V25/ReceiveEmergencyChat.cs
+++ AeroMessages/Matrix/V25/ReceiveEmergencyChat.cs
@@ -2,15 +2,16 @@ using System.Numerics;
 using System.Runtime.InteropServices;
 using Aero.Gen.Attributes;
 using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+using AeroMessages.Common;
 namespace AeroMessages.Matrix.V25
 {
     [Aero]
     [AeroMessageId(MsgType.Matrix, MsgSrc.Message, 67)]
     public partial class ReceiveEmergencyChat
     {
-        public ulong Unk1;
-        [AeroString] public string Unk2;
-        [AeroString] public string Unk3;
-        public uint Unk4;
+        public EntityId AuthorId;
+        [AeroString] public string DisplayName;
+        [AeroString] public string Message;
+        public uint ChatIconFlags; // Why a full uint tho
     }
 }
