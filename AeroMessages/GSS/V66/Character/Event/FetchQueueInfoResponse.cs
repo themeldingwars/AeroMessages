8dc66f499132848cf32861b6e3d53e2a0d7a0dbe
Xsear <Xsear@users.noreply.github.com>
2022-09-23 21:49:44 +0200

    Update FetchQueueInfoResponse

--- AeroMessages/GSS/V66/Character/Event/FetchQueueInfoResponse.cs
+++ AeroMessages/GSS/V66/Character/Event/FetchQueueInfoResponse.cs
@@ -6,66 +6,65 @@ namespace AeroMessages.GSS.V66.Character.Event
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 2, 145)]
     public partial class FetchQueueInfoResponse
     {
-        public byte Unk1;
-        [AeroArray(typeof(byte))] public FetchQueueData[] Data;
+        public byte Succes;
+        [AeroArray(typeof(byte))] public FetchQueueData[] Queues;
     }
 
     [AeroBlock]
     public struct FetchQueueData
     {
-        public uint Unk1;
-        public byte Unk2;
-        public byte Unk3;
-        [AeroString] public string Category;
-        [AeroString] public string TitleKey;
-        [AeroString] public string DescriptionKey;
+        public uint QueueId;
+        public byte Qualifies;
+        public byte ChallengeEnabled;
+        [AeroString] public string Gametype;
+        [AeroString] public string DisplayKeyName;
+        [AeroString] public string DisplayKeyDesc;
         public uint ZoneId;
-        public uint Unk8;
-        [AeroArray(typeof(byte))] public FetchQueueDataInner1[] ActivationCerts;
-        [AeroArray(typeof(byte))] public FetchQueueDataInner2[] DifficultiesDescription;
-        [AeroArray(typeof(byte))] public FetchQueueDataInner3[] Unk11;
-        [AeroArray(typeof(byte))] public FetchQueueDataInner4[] DifficultiesRewards;
-        [AeroArray(typeof(byte))] public FetchQueueDataInner3[] Unk13;
-        [AeroArray(typeof(byte))] public FetchQueueDataInner4[] Unk14; // Other rewards or just same parsing?
+        public uint MissionId;
+        [AeroArray(typeof(byte))] public QueueCertsData[] Certs;
+        [AeroArray(typeof(byte))] public QueueDifficultiesData[] Difficulties;
+        [AeroArray(typeof(byte))] public QueueRewardsItemData[] RewardsWinnerItems;
+        [AeroArray(typeof(byte))] public QueueRewardsLootData[] RewardsWinnerLoots;
+        [AeroArray(typeof(byte))] public QueueRewardsItemData[] RewardsLooserItems;
+        [AeroArray(typeof(byte))] public QueueRewardsLootData[] RewardsLooserLoots;
     }
 
     [AeroBlock]
-    public struct FetchQueueDataInner1
+    public struct QueueCertsData
     {
         [AeroSdb("dbitems::Certificate", "id")]
         public uint CertId;
-        public byte Unk2;
+        public byte Passed;
     }
 
     [AeroBlock]
-    public struct FetchQueueDataInner2
+    public struct QueueDifficultiesData
     {
-        public uint Unk1;
-        [AeroString] public string LabelKey;
-        public byte LevelRange1;
-        public byte LevelRange2;
-        public byte LevelRange3;
-        [AeroString] public string Difficulty;
+        public uint DifficultyId;
+        [AeroString] public string UiString;
+        public byte MinLevel;
+        public byte DisplayLevel;
+        public byte MaxSuggestedLevel;
+        [AeroString] public string DifficultyKey;
         public ushort PlayerCount1;
         public ushort PlayerCount2;
         public ushort PlayerCount3;
-        public ushort PlayerCount4; // Maybe min?
-        public ushort PlayerCount5;
+        public ushort MinPlayers;
+        public ushort MaxPlayers;
     }
 
     [AeroBlock]
-    public struct FetchQueueDataInner3
+    public struct QueueRewardsItemData
     {
         public uint Unk1;
         public uint Unk2;
     }
 
     [AeroBlock]
-    public struct FetchQueueDataInner4
+    public struct QueueRewardsLootData
     {
         [AeroSdb("dbitems::SalvageDisplayItems", "salvage_rewards_id")]
-        public uint RewardsId;
-
-        [AeroString] public string Difficulty;
+        public uint LootTableId;
+        [AeroString] public string DifficultyKey;
     }
 }
