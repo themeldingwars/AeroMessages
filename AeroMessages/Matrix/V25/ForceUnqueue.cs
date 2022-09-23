06eefa9349bc01c689a17033b5f9734808768c90
Xsear <Xsear@users.noreply.github.com>
2022-09-23 21:49:33 +0200

    Update ForceUnqueue

--- AeroMessages/Matrix/V25/ForceUnqueue.cs
+++ AeroMessages/Matrix/V25/ForceUnqueue.cs
@@ -2,14 +2,39 @@ using System.Numerics;
 using System.Runtime.InteropServices;
 using Aero.Gen.Attributes;
 using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+using AeroMessages.Common;
 namespace AeroMessages.Matrix.V25
 {
     [Aero]
     [AeroMessageId(MsgType.Matrix, MsgSrc.Message, 56)]
     public partial class ForceUnqueue
     {
-        public byte Unk1;
-        [AeroArray(typeof(byte))] public ulong[] Unk2;
-        public uint Unk3;
+        public enum QueueErrorReason : byte
+        {
+            Unknown = 0,
+            DidNotAccept = 1,
+            ServerError = 2,
+            InChallenge = 3,
+            NotEnoughPlayers = 4,
+            TooManyPlayers = 5,
+            InvalidTeamId = 6,
+            LackingAccess = 7,
+            TooManyPLayersPvP = 8,
+            CannotQueueFromThisInstance = 9,
+            InvalidQueues = 10,
+            NotSquadLeader = 11,
+            NotInSquad = 12,
+            SquadDoesNotExist = 13,
+            SquadReadyTimedOut = 14,
+            InvalidLFGCategory = 15,
+            TooManyQuitters = 16,
+            InvalidZoneId = 17,
+            MinimumLevelNotMet = 18,
+            DeserterDebuff = 19
+        }
+
+        public QueueErrorReason ReasonType;
+        [AeroArray(typeof(byte))] public EntityId[] CausingCharacters;
+        public uint ReasonValue;
     }
 }
