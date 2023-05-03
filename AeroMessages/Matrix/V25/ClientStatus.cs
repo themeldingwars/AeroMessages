43967df90f43c71fbdf6e0f02dde53c0236f7476
Xsear <Xsear@users.noreply.github.com>
2023-05-04 01:13:24 +0200

    Update Matrix ClientStatus

--- AeroMessages/Matrix/V25/ClientStatus.cs
+++ AeroMessages/Matrix/V25/ClientStatus.cs
@@ -2,19 +2,23 @@ using System.Numerics;
 using System.Runtime.InteropServices;
 using Aero.Gen.Attributes;
 using static Aero.Gen.Attributes.AeroMessageIdAttribute;
+using System;
+using AeroMessages.Common;
 namespace AeroMessages.Matrix.V25
 {
     [Aero]
     [AeroMessageId(MsgType.Matrix, MsgSrc.Command, 25)]
     public partial class ClientStatus
     {
-        public uint Unk1;
-        public ushort Unk2;
-        public ushort Unk3;
-        public uint Unk4;
-        public uint Unk5;
-        public ushort Unk6;
-        public ushort Unk7;
+        public uint Unk1; // Some form of uptime
+
+        public ushort RoundTripTime;
+        public ushort ElapsedMs;
+        public uint BytesDown;
+        public uint BytesUp;
+        public HalfFloat QualityOfServiceDown;
+        public HalfFloat QualityOfServiceUp;
+
         [AeroArray(typeof(ushort))] public byte[] Unk8; // FUN_016eed70, not 100%
     }
 }
