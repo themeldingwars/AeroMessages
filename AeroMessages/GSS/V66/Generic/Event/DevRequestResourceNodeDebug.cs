af400e5f483cf3d6fc87367e205804791bfd5a98
Xsear <Xsear@users.noreply.github.com>
2023-05-27 19:40:19 +0200

    Update GSS DevRequestResourceNodeDebug

--- AeroMessages/GSS/V66/Generic/Event/DevRequestResourceNodeDebug.cs
+++ AeroMessages/GSS/V66/Generic/Event/DevRequestResourceNodeDebug.cs
@@ -8,14 +8,6 @@ namespace AeroMessages.GSS.V66.Generic
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 0, 63)]
     public partial class DevRequestResourceNodeDebug
     {
-        [AeroArray(typeof(byte))] public DevReqResourceData[] Unk;
-    }
-
-    [AeroBlock]
-    public struct DevReqResourceData
-    {
-        public uint Unk1;
-        public uint Unk2;
-        public uint Unk3;
+        [AeroArray(typeof(byte))] public Vector3[] TestedPathLocations;
     }
 }
