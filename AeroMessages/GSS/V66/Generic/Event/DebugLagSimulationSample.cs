05762a534b3366b066124d6064048a3ae82d3e62
Xsear <Xsear@users.noreply.github.com>
2023-05-27 19:28:50 +0200

    Update GSS DebugLagSimulationSample

--- AeroMessages/GSS/V66/Generic/Event/DebugLagSimulationSample.cs
+++ AeroMessages/GSS/V66/Generic/Event/DebugLagSimulationSample.cs
@@ -8,18 +8,18 @@ namespace AeroMessages.GSS.V66.Generic
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 0, 95)]
     public partial class DebugLagSimulationSample
     {
-        public ushort Unk1;
-        public ushort Unk2;
-        public ushort Unk3;
-        public ushort Unk4;
-        public ushort Unk5;
-        public ushort Unk6;
-        public ushort Unk7;
-        public ushort Unk8;
-        public ushort Unk9;
-        public ushort Unk10;
-        public ushort Unk11;
-        public ushort Unk12;
-        public ushort Unk13;
+        public ushort Unk1; // Presumably short time but does not seem to be respected in that case.
+        public ushort SimulationDuration;
+        public ushort SimulatedObjects;
+        public ushort TotalObjects;
+        public ushort UpdateStarvedAverageMs;
+        public ushort UpdateStarvedMaximumMs;
+        public ushort LateUpdateAverageMs;
+        public ushort LateUpdateMaximumMs;
+        public ushort ReorederedObjects;
+        public ushort PriorityInversions;
+        public ushort DroppedPriorityUpdates;
+        public ushort UpdateRequests;
+        public ushort RejectedUpdateRequests;
     }
 }
