f7b795312b62db936e6280c575e1400eae334a3b
Xsear <Xsear@users.noreply.github.com>
2023-05-27 23:43:40 +0200

    Update DevRequestObjectPositions

--- AeroMessages/GSS/V66/Generic/Event/DevRequestObjectPositions.cs
+++ AeroMessages/GSS/V66/Generic/Event/DevRequestObjectPositions.cs
@@ -8,19 +8,11 @@ namespace AeroMessages.GSS.V66.Generic
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 0, 61)]
     public partial class DevRequestObjectPositions
     {
-        public byte Unk1;
+        public byte Show; // Seems to ignore stuff if 0 and will not print log unless 1.
         public byte Unk2;
-        [AeroArray(typeof(byte))] public DevReqObjectData[] Unk3;
+        [AeroArray(typeof(byte))] public Vector3[] Positions;
         [AeroArray(typeof(byte))] public float[] Unk4;
         [AeroArray(typeof(byte))] public float[] Unk5;
-        [AeroArray(typeof(byte))] public uint[] Unk6;
-    }
-
-    [AeroBlock]
-    public struct DevReqObjectData
-    {
-        public uint Unk1;
-        public uint Unk2;
-        public uint Unk3;
+        [AeroArray(typeof(byte))] public uint[] Unk6; // Presumably this would have been resource types?
     }
 }
