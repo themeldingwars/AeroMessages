46f4a88ea501ebc89d5347da12d18d2ebe4374c8
Xsear <Xsear@users.noreply.github.com>
2023-05-27 19:28:32 +0200

    Update GSS DebugLagPlayerSample

--- AeroMessages/GSS/V66/Generic/Event/DebugLagPlayerSample.cs
+++ AeroMessages/GSS/V66/Generic/Event/DebugLagPlayerSample.cs
@@ -8,10 +8,13 @@ namespace AeroMessages.GSS.V66.Generic
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 0, 94)]
     public partial class DebugLagPlayerSample
     {
-        public ushort Unk1;
-        public ushort Unk2;
-        public ushort Unk3;
-        public ushort Unk4;
-        public ushort Unk5;
+        // These three seem to be timestamps relative to the gameclock timer but also each other.
+        public ushort ShortTime_InputResponseReceivedAtMs; // Compared to InputRequestAt to find Server Player Input Delay
+        public ushort ShortTime_InputResponseSentAtMs; // Compared to clock to find Server Application Ping
+        public ushort ShortTime_InputRequestAtMs; // Compared to clock to find Client Application Ping (Client also uses PingTime on BaseController as data for this measurement)
+
+        // These two are regular values
+        public ushort ServerInputDelaySampleMs;
+        public ushort ServerLatenessMs;
     }
 }
