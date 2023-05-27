1abc9a651f86947d2a8a37245cf692cccb43ccfe
Xsear <Xsear@users.noreply.github.com>
2023-05-27 02:21:21 +0200

    Update DebugEventSample

--- AeroMessages/GSS/V66/Generic/Event/DebugEventSample.cs
+++ AeroMessages/GSS/V66/Generic/Event/DebugEventSample.cs
@@ -8,9 +8,24 @@ namespace AeroMessages.GSS.V66.Generic
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 0, 93)]
     public partial class DebugEventSample
     {
-        public ushort Unk1;
-        public ushort Unk2;
-        public byte Unk3;
-        public sbyte Unk4;
+        public enum DebugEventSampleType : byte
+        {
+            Frame = 0,
+            Update = 1,
+            WeaponInput_FireBurst = 2, // Green
+            WeaponInput_FireEnd = 3, // Red
+            WeaponInput_Reload = 4, // White
+            WeaponInput_Unk5 = 5, // Cyan, maybe Reload End?
+            WeaponInput_UseScope = 6, // Purple, could be mixed up
+            WeaponInput_SelectFireMode = 7, // Purple, could be mixed up
+            WeaponInput_SelectWeapon = 8, // Black
+            Weapon_Burst = 9, // DarkOrange
+            Weapon_FireWeaponProjectile = 10 // Orange
+        }
+
+        public ushort ShortTime;
+        public ushort Unk2; // Seems like another time value
+        public DebugEventSampleType Type;
+        public sbyte Queued; // 0 => "Server", else => "Server (Queued)"
     }
 }
