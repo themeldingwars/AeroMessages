0808215dc35d2ef05a6b7285974784b96702f57c
Xsear <Xsear@users.noreply.github.com>
2023-10-10 22:44:42 +0200

    Update DebugWeaponStats

--- AeroMessages/GSS/V66/Character/Event/DebugWeaponStats.cs
+++ AeroMessages/GSS/V66/Character/Event/DebugWeaponStats.cs
@@ -5,16 +5,16 @@ using static Aero.Gen.Attributes.AeroMessageIdAttribute;
 namespace AeroMessages.GSS.V66.Character.Event
 {
     [Aero]
-    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 5, 87)] // Uncertain controller
+    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 5, 87)]
     public partial class DebugWeaponStats
     {
-        public ushort Unk1;
-        public ushort Unk2;
-        public ushort Unk3;
-        public ushort Unk4;
-        public ushort Unk5;
-        public ushort Unk6;
-        public ushort Unk7;
-        public ushort Unk8;
+        public ushort Spawned;
+        public ushort Reported1;
+        public ushort Late;
+        public ushort Dropped;
+        public ushort Validated;
+        public ushort Succeeded1;
+        public ushort Reported2;
+        public ushort Succeeded2;
     }
 }
