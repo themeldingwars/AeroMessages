29987205380448ed31a718d65469c8ba33974e52
Xsear <Xsear@users.noreply.github.com>
2023-10-21 17:39:41 +0200

    Update ResourceNodeCompletedEvent

--- AeroMessages/GSS/V66/Character/Event/ResourceNodeCompletedEvent.cs
+++ AeroMessages/GSS/V66/Character/Event/ResourceNodeCompletedEvent.cs
@@ -7,25 +7,13 @@ namespace AeroMessages.GSS.V66.Character.Event
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 2, 137)]
     public partial class ResourceNodeCompletedEvent
     {
-        public ulong Unk1;
-        public ulong Unk2;
-        public ulong Unk3;
-        [AeroString] public string Unk4;
-        public float Unk5;
-        public float Unk6;
-        public byte Unk7;
-        public uint Unk8;
-        public byte Unk9;
-        public uint Unk10;
-        [AeroArray(typeof(byte))] public ResourceNodeCompletedInnerData[] Unk11;
-    }
-
-    [AeroBlock]
-    public struct ResourceNodeCompletedInnerData
-    {
-        // FUN_009df150
-        public uint Unk1;
-        public ushort Unk2;
-        public uint Unk3;
+        public EntityId ResourceNodeId;
+        public ThumpingCharacterInfoStruct ThumpingCharacterInfo; // Bit uncertain
+        public float Completion;
+        public byte Destroyed;
+        public uint ScanId;
+        public byte Unk9; // Empty?
+        public uint Quantity;
+        [AeroArray(typeof(byte))] public ResourceCompositionData[] Composition;
     }
 }
