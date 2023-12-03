63a8df0754dccfee529604b715ad8d489bb13b2c
Xsear <Xsear@users.noreply.github.com>
2023-12-03 03:50:14 +0100

    Update TimedDailyRewardRequest

--- AeroMessages/GSS/V66/Character/Command/TimedDailyRewardRequest.cs
+++ AeroMessages/GSS/V66/Character/Command/TimedDailyRewardRequest.cs
@@ -6,6 +6,14 @@ namespace AeroMessages.GSS.V66.Character.Command
     [AeroMessageId(MsgType.GSS, MsgSrc.Command, 2, 201)]
     public partial class TimedDailyRewardRequest
     {
-        public byte Unk1;
+        public enum RequestType : byte
+        {
+            // 00c0d0a0
+            ROLL = 0, // Player.RequestTimedDailyRewardRoll()
+            REROLL = 1,
+            RICKROLL = 2,
+            COMMIT = 3, // Player.RequestTimedDailyRewardCommit()
+        }
+        public RequestType Request;
     }
 }
