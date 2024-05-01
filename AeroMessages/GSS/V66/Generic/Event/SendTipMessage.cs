fb15222319ab437fd925f9f84f6110c662f07c02
Szymon Kamiński <kaminskisj@gmail.com>
2024-05-01 19:39:40 +0200

    Update UI-related messages

--- AeroMessages/GSS/V66/Generic/Event/SendTipMessage.cs
+++ AeroMessages/GSS/V66/Generic/Event/SendTipMessage.cs
@@ -1,18 +1,30 @@
 using Aero.Gen.Attributes;
 using static Aero.Gen.Attributes.AeroMessageIdAttribute;
 using AeroMessages.Common;
-using System.Numerics;
-using System;
 namespace AeroMessages.GSS.V66.Generic
 {
     [Aero]
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 0, 92)]
     public partial class SendTipMessage
-    {   
-        public byte Unk1;
-        public uint Unk2;
-        public uint Unk3;
-        public HalfFloat Unk4;
-        public sbyte Unk5;
+    {
+        public enum HintType : byte
+        {
+            None = 0,
+            Movement = 1,
+            TechSlot = 2
+        }
+
+        [AeroSdb("clientmissions::UiHints", "id")]
+        public HintType Hint;
+
+        [AeroSdb("dblocalization::LocalizedText", "id")]
+        public uint MessageId;
+
+        [AeroSdb("dblocalization::LocalizedText", "id")]
+        public uint HeaderId;
+
+        public HalfFloat Duration; // seconds
+
+        public sbyte Persistent;
     }
 }
