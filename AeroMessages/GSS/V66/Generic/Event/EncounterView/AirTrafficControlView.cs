0f31111a4e73fe2cb908337749c466dacc94fda3
Szymon Kamiński <kaminskisj@gmail.com>
2024-06-06 14:28:19 +0200

    Add few encounter views, extract Timer

---
+++ AeroMessages/GSS/V66/Generic/Event/EncounterView/AirTrafficControlView.cs
@@ -0,0 +1,12 @@
+﻿using Aero.Gen;
+using Aero.Gen.Attributes;
+
+namespace AeroMessages.GSS.V66.Generic.Event.EncounterView
+{
+    [Aero(AeroGenTypes.View)]
+    [AeroEncounter("AirTrafficControl")]
+    public partial class AirTrafficControlView
+    {
+        // has no fields
+    }
+}
---
+++ AeroMessages/GSS/V66/Generic/Event/EncounterView/HudTimerView.cs
@@ -0,0 +1,14 @@
+﻿using Aero.Gen;
+using Aero.Gen.Attributes;
+
+namespace AeroMessages.GSS.V66.Generic.Event.EncounterView
+{
+    [Aero(AeroGenTypes.View)]
+    [AeroEncounter("default")]
+    public partial class HudTimerView
+    {
+        private Timer hudtimer_timer;
+
+        private uint hudtimer_label;
+    }
+}
