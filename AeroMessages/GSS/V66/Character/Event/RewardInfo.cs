312b701a6cb591379fff4fbfa898e586bd3be16a
Xsear <Xsear@users.noreply.github.com>
2025-09-03 20:23:29 +0200

    Update RewardInfo

--- AeroMessages/GSS/V66/Character/Event/RewardInfo.cs
+++ AeroMessages/GSS/V66/Character/Event/RewardInfo.cs
@@ -7,11 +7,16 @@ namespace AeroMessages.GSS.V66.Character.Event
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 2, 88)]
     public partial class RewardInfo
     {
-        public byte RewardType;
-        public byte Bonus_RewardType; // Uncertain about this
-        public EntityId RelatedEntity;
-        public uint Unk_RewardValue; // Not the value that is displayed?
-        public uint Unk3;
-        public uint Bonus_RewardValue; // Matches expectations
+        public byte RewardType; // If not 0xf, isExp will be true.
+
+        public byte BonusType; // Part of a special condition when RelatedEntity is a Character and this is XP_REWARD_KILL or XP_REWARD_HEADSHOT, as well as some other condition that probably depend on the RelatedEntity. Maybe Audio related???
+
+        public EntityId RelatedEntity; // If set the UI tries to show the reward from this enemy
+
+        public uint RewardValue; // If this value is greater than BonusValue, the BonusValue is subtracted from it.
+
+        public uint KillCount; // If the reward type is XP_REWARD_KILL_STREAK or XP_REWARD_MULTIPLE_KILL, this is appended to the rewardType string.
+
+        public uint BonusValue; // If this is not 0, an additional UI event 0x71 is sent with rewardType XP_REWARD_BONUS, and this will be the rewardValue. isBonus will be set to 1. isExp will be based of the actual RewardType.
     }
 }
