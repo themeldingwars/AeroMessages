a53acb410c8b90401c166a5f7a866bda6de970a2
Xsear <Xsear@users.noreply.github.com>
2025-09-03 20:21:46 +0200

    Remove WeaponProjectileFired from CombatController
    Not present in capture

--- AeroMessages/GSS/V66/Character/Event/WeaponProjectileFired.cs
+++ AeroMessages/GSS/V66/Character/Event/WeaponProjectileFired.cs
@@ -4,7 +4,6 @@ using static Aero.Gen.Attributes.AeroMessageIdAttribute;
 namespace AeroMessages.GSS.V66.Character.Event
 {
     [Aero]
-    [AeroMessageId(MsgType.GSS, MsgSrc.Message, 5, 95)]
     [AeroMessageId(MsgType.GSS, MsgSrc.Message, 11, 95)]
     public partial class WeaponProjectileFired
     {
